f8f254193840c76b4a0fac690a32f336a7f7d20d
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-10-20 17:18:16 -0500

    Button of game to video implemented

--- PlatziGameStream/Vistas/HomeView.swift
+++ PlatziGameStream/Vistas/HomeView.swift
@@ -117,12 +117,12 @@ struct Home:View {
                 .background(Color("Blue-Gray"))
                 .clipShape(Capsule())
                 
+                ScrollView(showsIndicators: false){
+                    SubModuloHome()
+                }
             }.padding(.horizontal, 18.0)
             
         }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
-        
-        
- 
         
     }
     
@@ -139,7 +139,57 @@ struct Home:View {
     
 }
 
+struct SubModuloHome:View{
 
+    
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    @State var isPlayerActive = false
+    let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
+    
+    var body: some View{
+        VStack {
+            Text("LOS MÁS POPULARES")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                .padding(.top)
+            
+            ZStack{
+                Button {
+                    url = urlVideos[0]
+                    print("URL: \(url)")
+                    isPlayerActive = true
+                } label: {
+                    VStack(spacing: 0){
+                        Image("The Witcher 3")
+                            .resizable()
+                            .scaledToFill()
+                        Text("The Witcher 3: Wild Hunt")
+                            .frame(minWidth: 0,  maxWidth: .infinity,  alignment: .leading)
+                            .background(Color("Blue-Gray"))
+
+                    }
+                }
+                Image(systemName: "play.circle.fill")
+                    .resizable()
+                    .foregroundColor(.white)
+                    .frame(width: 42, height: 42)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+            .padding(.vertical)
+            
+            
+        }
+        NavigationLink(
+            destination: VideoPlayer(player: AVPlayer(url: URL(string: url)!))
+                .frame(width: 400, height: 300),
+            isActive: $isPlayerActive,
+            label:{
+                EmptyView()
+            })
+    }
+}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
