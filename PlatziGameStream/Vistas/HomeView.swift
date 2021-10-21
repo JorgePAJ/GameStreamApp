eb924c4f861bdcb96a2faf051541d311e47d7caa
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-10-21 11:50:07 -0500

    Added 2 ScrollsViews showing the categories of the games and some videos of 3 more games

--- PlatziGameStream/Vistas/HomeView.swift
+++ PlatziGameStream/Vistas/HomeView.swift
@@ -179,6 +179,91 @@ struct SubModuloHome:View{
             .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
             .padding(.vertical)
             
+            Text("CATEGORÍAS SUGERIDAS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    Button(action: {}) {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("FPS")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    Button(action: {}) {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("RPG")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                    Button(action: {}) {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(Color("Blue-Gray"))
+                                .frame(width: 160, height: 90)
+                            Image("OpenWorld")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 42, height: 42)
+                        }
+                    }
+                }
+            }
+            
+            Text("RECOMENDADOS PARA TI")
+                .font(.title3)
+                .foregroundColor(.white)
+                .bold()
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal, showsIndicators: false){
+                HStack{
+                    Button {
+                        url = urlVideos[1]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    } label: {
+                        Image("Abzu")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        }
+                    Button {
+                        url = urlVideos[2]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    } label: {
+                        Image("Crash Bandicoot")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        }
+                    Button {
+                        url = urlVideos[3]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    } label: {
+                        Image("DEATH STRANDING")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 240, height: 135)
+                        }
+                }
+            }
+            
             
         }
         NavigationLink(
