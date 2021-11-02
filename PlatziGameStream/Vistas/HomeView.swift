32b2ced056e503463c50ab7cc042093266c12cc0
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-11-02 13:48:59 -0600

    Added videos you might like section to homeview

--- PlatziGameStream/Vistas/HomeView.swift
+++ PlatziGameStream/Vistas/HomeView.swift
@@ -1,70 +1,64 @@
-//
-//  HomeView.swift
-//  PlatziGameStream
-//
-//
-
 import SwiftUI
 import AVKit
 
 struct HomeView: View {
-   
-  
+    
+    
+    
     @State var tabSeleccionado:Int = 2
     
     var body: some View {
         
         
-               
-                TabView(selection:$tabSeleccionado) {
-                   
-                    
-                    
+        
+        TabView(selection:$tabSeleccionado) {
+            
+            
+            
+            Text("Perfil")
+                .font(.system(size: 30, weight: .bold, design: .rounded)).navigationBarHidden(true).navigationBarBackButtonHidden(true)
+                .tabItem {
+                    Image(systemName: "person")
                     Text("Perfil")
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
-                        .tabItem {
-                            Image(systemName: "person")
-                            Text("Perfil")
-                        }.tag(0)
-                        
-                 
-                    GamesView()
-                        .tabItem {
-                            Image(systemName: "gamecontroller")
-                            Text("Juegos")
-                        }.tag(1)
-                    
-                    
-                    Home().tabItem {
-                        Image(systemName: "house")
-                        Text("Inicio")
-                    }.tag(2)
-                    
-                    
-                   
-                    
-                   
+                }.tag(0)
+            
+            
+            GamesView()
+                .tabItem {
+                    Image(systemName: "gamecontroller")
+                    Text("Juegos")
+                }.tag(1)
+            
+            
+            Home().tabItem {
+                Image(systemName: "house")
+                Text("Inicio")
+            }.tag(2)
+            
+ 
+            
+            Text("Favoritos")
+                .font(.system(size: 30, weight: .bold, design: .rounded))
+                .tabItem {
+                    Image(systemName: "heart")
                     Text("Favoritos")
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
-                        .tabItem {
-                            Image(systemName: "heart")
-                            Text("Favoritos")
-                        }.tag(3)
-                    
-                 
-                 
-                    
-                 
-                   
-                }
-                .accentColor(.white)
-                
-                
-                
-                
-                
-     
-         
+                }.tag(3)
+            
+           
+            
+            
+            
+            
+            
+        }
+        .accentColor(.white)
+        
+        
+        
+        
+        
+        
+        
     }
     
     
@@ -78,214 +72,338 @@ struct HomeView: View {
     }
     
     
-   
+    
 }
 
 
-//Puede llamarse Home o PantallaHome, siempre y cuando describa la estructura y su motivo de ser.
+
 struct Home:View {
     
-   
     
-
+    
     
     var body: some View{
-    
-
+        
+        
         ZStack{
+            
             Color("Marine").ignoresSafeArea()
+            
+            
+            
             VStack{
-                Image("AppLogo").resizable()
-                     .aspectRatio(contentMode: .fit)
-                     .frame(width: 250)
-                     .padding(.horizontal, 11.0)
-
+                
+                
+                
+                Image("AppLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.vertical, 11.0)
+                
                 
                 ScrollView(showsIndicators: false){
                     SubModuloHome()
+                    
                 }
+                
+                
             }.padding(.horizontal, 18.0)
             
-        }
-        .navigationBarHidden(true)
-        .navigationBarBackButtonHidden(true)
+            
+            
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
+        
+        
+        
+        
+        
+        
+        
+        
     }
-
     
-//    func buscar() {
-//        print("Buscando video que contenga \(textoBusqueda)")
-//    }
+    
     
 }
 
-struct SubModuloHome:View{
-    @State var isGameInfoEmpty = false
-    @State var textoBusqueda = ""
+
+struct SubModuloHome:View {
+    
+    @State var textoBusqueda:String = ""
     @ObservedObject var juegoEncontrado = SearchGame()
     @State var isGameViewActive = false
+    @State var isGameInfoEmpty = false
     @State var url:String = ""
     @State var titulo:String = ""
     @State var studio:String = ""
-    @State var calificacion: String = ""
-    @State var anoPublicacion: String = ""
+    @State var calificacion:String = ""
+    @State var anoPublicacion:String = ""
     @State var descripcion:String = ""
     @State var tags:[String] = [""]
-    @State var imgsUrl: [String] = [""]
+    @State var imgsUrl:[String] = [""]
+    
+    
+    
     
     var body: some View{
-        VStack {
-            HStack{
-                Button(action: {
-                    
-                    watchGame(name: textoBusqueda)
-                    
-                    
-                }) {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(textoBusqueda.isEmpty ? Color(.white) : Color("Dark-Cyan"))
-                }.alert(isPresented: $isGameInfoEmpty){
-                    
+        
+        VStack{
+            
+            
+            
+            HStack {
+                
+                Button(action: {watchGame(name: textoBusqueda)}, label: {
+                    Image(systemName: "magnifyingglass").foregroundColor(textoBusqueda.isEmpty ? Color.white : Color("Dark-Cyan") )
+                }).alert(isPresented: $isGameInfoEmpty) {
                     Alert(title: Text("Error"), message: Text("No se encontro el juego"), dismissButton: .default(Text("Entendido")))
+                }
+                
+                
+                ZStack(alignment: .leading){
+                    
+                    
+                    
+                    if textoBusqueda.isEmpty { Text("Buscar un video").foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1.0)) }
+                    
+                    
+                    TextField("", text: $textoBusqueda).foregroundColor(.white)
+                    
                     
                 }
-                ZStack(alignment: .leading){
-                    if textoBusqueda.isEmpty{
-                        Text("Buscar un video")
-                            .foregroundColor(Color(red: 174/255, green: 177/255, blue: 185/255, opacity: 1))
-                    }
-                    TextField("",text: $textoBusqueda)
-                        .foregroundColor(.white)
-                }
-            }
-            .padding([.top,.leading,.bottom],11)
+                
+                
+                
+                
+            }.padding([.top, .leading, .bottom], 11.0)
             .background(Color("Blue-Gray"))
             .clipShape(Capsule())
-            Text("LOS MÁS POPULARES")
-                .font(.title3)
-                .foregroundColor(.white)
-                .bold()
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            
+            
+            
+            
+            
+            
+            
+            
+            Text("LOS MÁS POPULARES").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
                 .padding(.top)
             
             ZStack{
-                Button {
-                    watchGame(name: "The Witcher 3")
-                } label: {
-                    VStack(spacing: 0){
-                        Image("The Witcher 3")
-                            .resizable()
-                            .scaledToFill()
-                        Text("The Witcher 3: Wild Hunt")
-                            .frame(minWidth: 0,  maxWidth: .infinity,  alignment: .leading)
-                            .background(Color("Blue-Gray"))
-
+                
+                
+                
+                Button( action: {watchGame(name: "The Witcher 3")}, label: {
+                    
+                    VStack(spacing: 0) {
+                        
+                        Image("The Witcher 3").resizable().scaledToFill()
+                        
+                        
+                        Text("The Witcher 3: Wild Hunt").frame(minWidth: 0,  maxWidth: .infinity,  alignment: .leading).background(Color("Blue-Gray"))
+                        
+                        
+                        
                     }
-                }
-                Image(systemName: "play.circle.fill")
-                    .resizable()
-                    .foregroundColor(.white)
-                    .frame(width: 42, height: 42)
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    
+                    
+                })
+                
+                
+                
+                Image(systemName: "play.circle.fill").resizable().foregroundColor(.white).frame(width: 42.0, height: 42.0)
+                
+            }.frame(minWidth: 0, maxWidth: .infinity,alignment: .center)
             .padding(.vertical)
             
-            Text("CATEGORÍAS SUGERIDAS PARA TI")
-                .font(.title3)
-                .foregroundColor(.white)
-                .bold()
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
             
-            ScrollView(.horizontal, showsIndicators: false){
+            Text("CATEGORÍAS SUGERIDAS PARA TI").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+            
+            
+            ScrollView(.horizontal,showsIndicators: false){
+                
+                
                 HStack{
-                    Button(action: {}) {
+                    
+                    Button(action: {}, label: {
+                        
                         ZStack{
+                            
                             RoundedRectangle(cornerRadius: 8)
                                 .fill(Color("Blue-Gray"))
                                 .frame(width: 160, height: 90)
-                            Image("FPS")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 42, height: 42)
+                            
+                            Image("FPS").resizable().scaledToFit().frame(width: 42, height: 42)
+                            
+                            
+                            
                         }
-                    }
-                    Button(action: {}) {
+                        
+                    })
+                    
+                    Button(action: {}, label: {
+                        
                         ZStack{
+                            
                             RoundedRectangle(cornerRadius: 8)
                                 .fill(Color("Blue-Gray"))
                                 .frame(width: 160, height: 90)
-                            Image("RPG")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 42, height: 42)
+                            
+                            Image("RPG").resizable().scaledToFit().frame(width: 42, height: 42)
+                            
+                            
+                            
                         }
-                    }
-                    Button(action: {}) {
+                        
+                    })
+                    
+                    Button(action: {}, label: {
+                        
                         ZStack{
+                            
                             RoundedRectangle(cornerRadius: 8)
                                 .fill(Color("Blue-Gray"))
                                 .frame(width: 160, height: 90)
-                            Image("OpenWorld")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 42, height: 42)
+                            
+                            Image("OpenWorld").resizable().scaledToFit().frame(width: 42, height: 42)
+                            
+                            
+                            
                         }
-                    }
+                        
+                    })
                 }
             }
             
-            Text("RECOMENDADOS PARA TI")
-                .font(.title3)
-                .foregroundColor(.white)
-                .bold()
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            Text("RECOMENDADOS PARA TI").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
             
-            ScrollView(.horizontal, showsIndicators: false){
+            ScrollView(.horizontal,showsIndicators: false){
+                
+                
                 HStack{
-                    Button {
-                        watchGame(name: "Abzu")
-                    } label: {
-                        Image("Abzu")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 240, height: 135)
-                        }
-                    Button {
-                        watchGame(name: "Crash Bandicoot")
-                    } label: {
-                        Image("Crash Bandicoot")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 240, height: 135)
-                        }
-                    Button {
-                        watchGame(name: "DEATH STRANDING")
-                    } label: {
-                        Image("DEATH STRANDING")
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 240, height: 135)
-                        }
+                    
+                    Button(action: {watchGame(name: "Abzu")}, label: {
+                        
+                        
+                        
+                        Image("Abzu").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
+                    
+                    Button(action: {watchGame(name: "Crash Bandicoot")}, label: {
+                        
+                        
+                        
+                        Image("Crash Bandicoot").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
+                    
+                    
+                    Button(action: {watchGame(name: "DEATH STRANDING")}, label: {
+                        
+                        
+                        
+                        Image("DEATH STRANDING").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
                 }
             }
+            
+            
+            
+            Text("VIDEOS QUE PODRIAN GUSTARTE").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+            
+            ScrollView(.horizontal,showsIndicators: false){
+                
+                
+                HStack{
+                    
+                    Button(action: {watchGame(name: "Cuphead")}, label: {
+                        
+                        
+                        
+                        Image("Cuphead").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
+                    
+                    Button(action: {watchGame(name: "Hades")}, label: {
+                        
+                        
+                        
+                        Image("Hades").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
+                    
+                    
+                    Button(action: {watchGame(name: "Grand Theft Auto V")}, label: {
+                        
+                        
+                        
+                        Image("Grand Theft Auto V").resizable().scaledToFit().frame(width: 240, height: 135)
+                        
+                        
+                        
+                        
+                    })
+                }
+            }
+            
+            
+            // para agregar insets a scrollview
+            Color.clear.frame(width: 1, height: 8, alignment: .center)
             
             
         }
+        
+        
         NavigationLink(
-            destination: GameView(url: url, titulo: titulo, studio: studio, calificacion: calificacion, anoPublicacion: anoPublicacion, descripcion: descripcion, tags: tags, imgsUrl: imgsUrl),
+            destination: GameView(url: url, titulo: titulo, studio: studio, calificacion: calificacion, anoPublicacion: anoPublicacion, descripcion: descripcion, tags: tags, imgsUrl: imgsUrl)
+            ,
             isActive: $isGameViewActive,
-            label:{
+            label: {
+                
+                
                 EmptyView()
             })
         
+        
+        
+        
     }
-    func watchGame(name:String) {
+    
+    
+    
+    
+    
+    func watchGame(name:String)  {
+        
         juegoEncontrado.search(gameName: name)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
-            print("Cantidad E: \(juegoEncontrado.gameInfo.count)")
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+0.1) {
             
+            
+            print("Cantidad E: \(juegoEncontrado.gameInfo.count)")
             if juegoEncontrado.gameInfo.count == 0{
+                
+                print("No se encontro ningun juego llamado \(name)")
+                
                 isGameInfoEmpty = true
+                
             }else{
+                
                 url = juegoEncontrado.gameInfo[0].videosUrls.mobile
                 titulo = juegoEncontrado.gameInfo[0].title
                 studio = juegoEncontrado.gameInfo[0].studio
@@ -295,13 +413,19 @@ struct SubModuloHome:View{
                 tags = juegoEncontrado.gameInfo[0].tags
                 imgsUrl = juegoEncontrado.gameInfo[0].galleryImages
                 
-                isGameViewActive = true
                 
+                isGameViewActive = true
             }
         }
+        
+        
+        
+        
     }
-
+    
+    
 }
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
