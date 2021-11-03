1858a9ff3a0e59f18a3480bc3da853c32872a830
Jorge Plasencia <66725751+WaberHoruhe@users.noreply.github.com>
2021-11-03 14:24:17 -0600

    podfiles
    added the podfiles to repo

---
+++ PlatziGameStream/Modelos/Model.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+struct Resultados:Codable{
+    var results:[Game]
+}
+
+struct Games:Codable {
+    var games:[Game]
+}
+
+
+struct Game:Codable, Hashable {
+    
+    
+    var title:String
+    var studio:String
+    var contentRaiting:String
+    var publicationYear:String
+    var description:String
+    var platforms:[String]
+    var tags:[String]
+    var videosUrls:videoUrl
+    var galleryImages:[String]
+        
+    
+}
+
+
+struct videoUrl:Codable, Hashable {
+   
+    var mobile:String
+    var tablet:String
+    
+}
